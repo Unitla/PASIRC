2605a09004bf9aff62f7d0e664e8e997ed96f9cc
Unitla <szergg@gmail.com>
2018-06-25 19:48:08 +0200

    Unable to commend handler

--- test.py
+++ test.py
@@ -85,19 +85,27 @@ def command_quit():
     return ''.join(["QUIT:", nick])
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.verify_mode = ssl.CERT_REQUIRED
-context.load_verify_locations('cert.pem')
-# context.load_cert_chain(certfile='./certs/client/client.crt', keyfile="./certs/client/client.key")
-if ssl.HAS_SNI:
-    secure_sock = context.wrap_socket(sock, server_hostname=HOST)
-else:
-    secure_sock = context.wrap_socket(sock)
-cert = secure_sock.getpeercert()
-if not cert or ssl.match_hostname(cert, HOST):
-    raise Exception("Error")
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_verify_locations('cert.pem')
+    # context.load_cert_chain(certfile='./certs/client/client.crt', keyfile="./certs/client/client.key")
+    if ssl.HAS_SNI:
+        secure_sock = context.wrap_socket(sock, server_hostname=HOST)
+    else:
+        secure_sock = context.wrap_socket(sock)
+    cert = secure_sock.getpeercert()
+    if not cert or ssl.match_hostname(cert, HOST):
+        raise Exception("Error")
+except Exception as err:
+    mGuiClose = tk.Tk()
+    mGuiClose.title("Error")
+    mlabel = tk.Label(text="Socket connection could not be made error message "+str(err))
+    mlabel.pack()
+    mGuiClose.mainloop()
+    quit(1)
 
 # secure_sock.send("LOGIN:admin@haslo")
 # print recv_all(secure_sock, "\r\n")
